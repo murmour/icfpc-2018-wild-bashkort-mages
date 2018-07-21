20e28cf519a1f17fb27398f44f8d2da5e4c7fc8e
Max Mouratov <mmouratov@gmail.com>
2018-07-21 18:34:08 +0500

    Параллельный комбайн

--- manipulator/runner.py
+++ manipulator/runner.py
@@ -1,5 +1,6 @@
 
 import subprocess
+import time
 import re
 import json
 import sys
@@ -31,44 +32,72 @@ def filter_problems(lowIndex, highIndex):
     return ps
 
 
-def run_solver(executable, problem_file, trace_file, solver_name, rest_args):
-    print('Producing %s...' % trace_file)
-    temp_file = 'tmp'
-    p = subprocess.Popen([ executable,
-                           '-in', problem_file,
-                           '-out', temp_file,
-                           '-solver', solver_name ] + rest_args,
-                         stdin=subprocess.PIPE,
-                         stdout=subprocess.PIPE)
-    out = p.communicate()[0].decode()
-    if p.returncode != 0:
-        print('Error! (return code %d)' % p.returncode)
-        return None
-    else:
-        energy = int(out)
-        print('Success! (energy %d)' % energy)
-        os.rename(temp_file, trace_file)
-        return energy
+temp_counter = 0
 
 
 if __name__ == '__main__':
     if len(sys.argv) < 5:
-        print('Usage: runner.py executable solver lowIndex highIndex')
+        print('Usage: runner.py executable solver lowIndex highIndex cpus')
         sys.exit(1)
 
     executable = sys.argv[1]
     solver = sys.argv[2]
     low_index = int(sys.argv[3])
     high_index = int(sys.argv[4])
+    cpus = int(sys.argv[5])
 
-    for p in filter_problems(low_index, high_index):
-        trace_base = '../data/traces/%s%s_%s' % (p['prefix'], p['id'], solver)
-        trace_file = trace_base + '.nbt'
-        meta_file = trace_base + '.meta'
-        if os.path.isfile(meta_file):
+    def start_solving(p):
+        global temp_counter
+        worker = {}
+        trace_base = 'local_traces/%s%s_%s' % (p['prefix'], p['id'], solver)
+        # trace_base = '../data/traces/%s%s_%s' % (p['prefix'], p['id'], solver)
+        worker['trace_file'] = trace_base + '.nbt'
+        worker['meta_file'] = trace_base + '.meta'
+        if os.path.isfile(worker['meta_file']):
             print('Trace %s already exists.' % trace_base)
+            return None
+        print('Producing %s...' % worker['trace_file'])
+        worker['temp_file'] = 'temp/tmp%d' % temp_counter
+        temp_counter += 1
+        worker['process'] = subprocess.Popen([ executable,
+                                             '-in', p['fname'],
+                                             '-out', worker['temp_file'],
+                                             '-solver', solver ],
+                                           stdin=subprocess.PIPE,
+                                           stdout=subprocess.PIPE)
+        return worker
+
+    def finish_solving(worker, retcode):
+        if retcode != 0:
+            print('%s: ERROR (return code %d)' % (worker['trace_file'], retcode))
         else:
-            energy = run_solver(executable, p['fname'], trace_file, solver, [])
-            if energy != None:
-                with io.open(meta_file, 'w') as f:
-                    f.write(json.dumps({ 'energy': energy }))
+            out = worker['process'].communicate()[0].decode()
+            energy = int(out)
+            print('%s: SUCCESS (energy %d)' % (worker['trace_file'], energy))
+            os.rename(worker['temp_file'], worker['trace_file'])
+            with io.open(worker['meta_file'], 'w') as f:
+                f.write(json.dumps({ 'energy': energy }))
+
+    pool = [ None ] * cpus
+    queue = filter_problems(low_index, high_index)
+    left = len(queue)
+
+    while left > 0:
+        time.sleep(0.1)
+        for i in range(len(pool)):
+            if pool[i] is None:
+                if queue == []:
+                    continue
+                p = queue[0]
+                queue = queue[1:]
+                pool[i] = start_solving(p)
+                if pool[i] is None:
+                    continue
+                else:
+                    break
+            worker = pool[i]
+            retcode = worker['process'].poll()
+            if retcode is not None:
+                left -= 1
+                pool[i] = None
+                finish_solving(worker, retcode)
