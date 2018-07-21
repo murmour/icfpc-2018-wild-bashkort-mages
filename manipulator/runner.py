cfde9b22eab4e084f4562a77c8a4db29427ed543
Max Mouratov <mmouratov@gmail.com>
2018-07-21 18:39:06 +0500

    runner: local_traces -> data/traces

--- manipulator/runner.py
+++ manipulator/runner.py
@@ -49,8 +49,8 @@ if __name__ == '__main__':
     def start_solving(p):
         global temp_counter
         worker = {}
-        trace_base = 'local_traces/%s%s_%s' % (p['prefix'], p['id'], solver)
-        # trace_base = '../data/traces/%s%s_%s' % (p['prefix'], p['id'], solver)
+        # trace_base = 'local_traces/%s%s_%s' % (p['prefix'], p['id'], solver)
+        trace_base = '../data/traces/%s%s_%s' % (p['prefix'], p['id'], solver)
         worker['trace_file'] = trace_base + '.nbt'
         worker['meta_file'] = trace_base + '.meta'
         if os.path.isfile(worker['meta_file']):
