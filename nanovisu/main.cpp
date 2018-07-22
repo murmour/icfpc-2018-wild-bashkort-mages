3ec461c70d251533bddfb83a326f76357f009c98
Max Mouratov <mmouratov@gmail.com>
2018-07-22 11:16:44 +0500

    nanovisu: list sorting

--- nanovisu/main.cpp
+++ nanovisu/main.cpp
@@ -410,6 +410,7 @@ void refresh_list_of_model_files()
 				model_files.push_back( string(ent->d_name) );
 		closedir(dir);
 	}
+    std::sort(model_files.begin(), model_files.end());
 }
 
 vector< string > parse_filename( string x )
@@ -518,6 +519,7 @@ void load_model_file( string file )
 	cur_trace = "not selected";
 	trace_cmd.clear();
 	cerr << "ok! traces=" << trace_files.size() << "\n";
+    std::sort(trace_files.begin(), trace_files.end());
 }
 
 void load_trace_file( string file )
