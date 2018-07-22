d30f74f48e530183f025b9f917c77b2d650edbe2
Max Mouratov <mmouratov@gmail.com>
2018-07-23 03:10:10 +0500

    solver: R-s (unfinished)

--- Solver/main.cpp
+++ Solver/main.cpp
@@ -20,37 +20,14 @@ int main(int argc, char** argv)
 	auto in_file = System::GetArgValue("in");
 
 	char ptype = get_type(in_file);
-	//if (ptype != 'A') return 42;
 
-	string in_file2;
-	if (ptype == 'R')
-	{
-		auto idx = in_file.find("src");
-		if (idx == string::npos) return 66;
-		in_file2 = in_file;
-		in_file2.replace(idx, 3, "tgt");
-	}
 	Matrix *model = new Matrix();
-	Matrix *model2 = nullptr;
-
 	if (!model->load_from_file(in_file.c_str()))
 	{
 		fprintf(stderr, "Failed to load model '%s'", in_file.c_str());
 		return 1;
 	}
 	model->init_sums();
-
-	if (!in_file2.empty())
-	{
-		model2 = new Matrix();
-		if (!model2->load_from_file(in_file2.c_str()))
-		{
-			fprintf(stderr, "Failed to load model2 '%s'", in_file2.c_str());
-			return 1;
-		}
-		Assert(model->R == model2->R);
-		model2->init_sums();
-	}
 
 	string solver = "bfs"; // default solver
 	if (System::HasArg("solver"))
@@ -63,7 +40,7 @@ int main(int argc, char** argv)
 	}
 
 	if (ptype == 'A')
-    {
+	{
 		FileTraceWriter *tw = new FileTraceWriter(out_file.c_str(), model->R, nullptr);
 		solver_f(nullptr, model, tw);
 
@@ -74,10 +51,11 @@ int main(int argc, char** argv)
 		printf("%lld", tw->get_energy()); // print total energy spent
 		delete tw;
 		return 0;
-    }
+	}
 
 	else if (ptype == 'D')
-    {
+	{
+		// Construction
 		const string rev_out_file = out_file + ".rev";
 		FileTraceWriter *tw = new FileTraceWriter(rev_out_file.c_str(), model->R, nullptr);
 		solver_f(nullptr, model, tw);
@@ -86,50 +64,78 @@ int main(int argc, char** argv)
 		Assert(tw->get_filled_count() == model->get_filled_count());
 		delete tw;
 
+		// Reversing construction to deconstruction
 		tw = new FileTraceWriter(out_file.c_str(), model->R, model);
-        reverse_trace(rev_out_file, tw);
-        tw->halt();
+		reverse_trace(rev_out_file, tw);
+		tw->halt();
 		Assert(tw->get_filled_count() == 0);
 
-		printf("%lld", tw->get_energy()); // print total energy spent
+		printf("%lld", tw->get_energy());
 		delete tw;
 		return 0;
 	}
 
-	// todo
-    else if (ptype == 'R')
+	else if (ptype == 'R')
 	{
-		FileTraceWriter *tw = new FileTraceWriter(out_file.c_str(), model->R, model);
+		auto idx = in_file.find("src");
+		if (idx == string::npos) return 66;
+		string tgt_file = in_file;
+		tgt_file.replace(idx, 3, "tgt");
 
-		if (model->check_equal(*model2))
+		Matrix *tgt_model = new Matrix();
+		if (!tgt_model->load_from_file(tgt_file.c_str()))
 		{
-			// no operations needed
+			fprintf(stderr, "Failed to load tgt_model '%s'", tgt_file.c_str());
+			return 1;
 		}
-		else if (System::HasArg("solver2"))
+		Assert(model->R == tgt_model->R);
+		tgt_model->init_sums();
+
+		string tgt_solver = solver;
+		if (System::HasArg("solver2"))
+			tgt_solver = System::GetArgValue("solver2");
+		auto tgt_solver_f = GetSolver(solver);
+		if (!tgt_solver_f)
 		{
-			auto solver2 = System::GetArgValue("solver2");
-			auto solver2_f = GetSolver(solver2);
-			if (!solver2_f)
-			{
-				fprintf(stderr, "Unsupported solver: %s", solver2.c_str());
-				return 4;
-			}
-			solver_f(model, nullptr, tw);
-			solver2_f(nullptr, model2, tw);
-		}
-		else
-		{
-			solver_f(model, model2, tw);
+			fprintf(stderr, "Unsupported solver: %s", tgt_solver.c_str());
+			return 3;
 		}
 
+		// Equivalent models -> fast path
+		if (model->check_equal(*tgt_model))
+		{
+			FileTraceWriter *tw = new FileTraceWriter(tgt_file.c_str(), tgt_model->R, tgt_model);
+			tw->halt();
+			printf("%lld", tw->get_energy());
+			delete tw;
+			return 0;
+		}
+
+		// Construction
+		const string rev_out_file = out_file + ".rev";
+		FileTraceWriter *tw = new FileTraceWriter(rev_out_file.c_str(), model->R, nullptr);
+		solver_f(nullptr, model, tw);
 		tw->halt();
 		Assert(tw->get_matrix().check_equal(*model));
 		Assert(tw->get_filled_count() == model->get_filled_count());
+		delete tw;
 
-		printf("%lld", tw->get_energy()); // print total energy spent
+		// Reversing construction to deconstruction
+		tw = new FileTraceWriter(out_file.c_str(), model->R, model);
+		reverse_trace(rev_out_file, tw);
+		Assert(tw->get_filled_count() == 0);
+
+		// Construction
+		tgt_solver_f(nullptr, tgt_model, tw);
+		tw->halt();
+		Assert(tw->get_matrix().check_equal(*tgt_model));
+		Assert(tw->get_filled_count() == tgt_model->get_filled_count());
+
+		printf("%lld", tw->get_energy());
 		delete tw;
 		return 0;
 	}
+
 	else
 		return 44;
 }
