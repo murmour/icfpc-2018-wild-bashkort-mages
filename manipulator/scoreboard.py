8a95f63ff29d3066c8a4406cb0a9ec7648ebc197
Max Mouratov <mmouratov@gmail.com>
2018-07-23 13:36:09 +0500

    YOPT

--- manipulator/scoreboard.py
+++ manipulator/scoreboard.py
@@ -2,14 +2,16 @@
 import io
 import os
 import common
+import sys
 
 
 if __name__ == '__main__':
-    ps = common.get_all_problems()
+    ps = common.filter_problems(0, 200, sys.argv[1])
     ps_a = [ p for p in ps if p['prefix'] == 'FA' ]
     ps_d = [ p for p in ps if p['prefix'] == 'FD' ]
     ps_r = [ p for p in ps if p['prefix'] == 'FR' ]
-    ps = ps_a + ps_d + ps_r
+    ps_u = [ p for p in ps if p['prefix'] == 'FU' ]
+    ps = ps_a + ps_d + ps_r + ps_u
 
     ts = common.get_all_good_traces()
 
